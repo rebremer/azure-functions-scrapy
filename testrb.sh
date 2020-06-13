d0173013411e75f13b0cad7f678d8aa2565f1a65
René Bremer <32130834+rebremer@users.noreply.github.com>
2020-06-13 13:43:03 +0200

    Update testrb.sh

--- testrb.sh
+++ testrb.sh
@@ -1,2 +1,3 @@
 # hadoop distcp abfs://testdata@testhdistor.dfs.core.windows.net/ wasbs://dest@testhdistorblob.blob.core.windows.net/
-hadoop distcp wasbs://sou@thso.blob.core.windows.net/ wasbs://sin@$thsi.blob.core.windows.net/
+# hadoop distcp wasbs://sou@thso.blob.core.windows.net/ wasbs://sin@$thsi.blob.core.windows.net/
+hadoop distcp wasbs://sou@thso.blob.core.windows.net/ wasbs://sin@thsi.blob.core.windows.net/
